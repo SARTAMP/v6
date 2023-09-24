13de80554b6fa9f063095f26ad6dce95877bb87f
SARTAMP <139582244+SARTAMP@users.noreply.github.com>
2023-09-24 23:20:45 +0700

    Update add-vless.sh

--- add-vless.sh
+++ add-vless.sh
@@ -170,10 +170,6 @@ echo -e "Link Grpc => ${vlesslink3}"
 echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"                
 echo -e "Expired => $exp"
 echo -e "${BIBlue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e ""
-echo -e "┌─────────────────────────────────────────────────┐" | lolcat
-echo -e "│               YSSHSTORE                   │" | lolcat
-echo -e "└─────────────────────────────────────────────────┘" | lolcat   
 echo -e ""             
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
